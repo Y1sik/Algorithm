1413c8b462205ab7739422d76a311bf8f4424e3c
우윤식 <yun0522v@naver.com>
2022-03-04 10:04:28 +0900

    Add 구간합구하기5

---
+++ BJ/BJ_구간합구하기5.java
@@ -0,0 +1,53 @@
+package BJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_구간합구하기5 {
+	static int N;
+	static int M;
+	static int x1, x2, y1, y2;
+	static int[][] map;
+	static int[][] dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		map = new int[N + 1][N + 1];
+		dp = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + map[i][j];
+			}
+		}
+
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			x1 = Integer.parseInt(st.nextToken());
+			y1 = Integer.parseInt(st.nextToken());
+			x2 = Integer.parseInt(st.nextToken());
+			y2 = Integer.parseInt(st.nextToken());
+
+			Prefix_Sum(x1, y1, x2, y2);
+		}
+	}
+
+	private static void Prefix_Sum(int x1, int y1, int x2, int y2) {
+		int result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+		System.out.println(result);
+	}
+
+}
