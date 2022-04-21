66eec06c654f8787be9ba3f1bb63c375e6fd9e44
우윤식 <yun0522v@naver.com>
2022-04-21 23:35:54 +0900

    Update 톱니바퀴

--- BJ/BJ_톱니바퀴.java
+++ BJ/BJ_톱니바퀴.java
@@ -33,9 +33,9 @@ public class BJ_톱니바퀴 {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			index = Integer.parseInt(st.nextToken()) - 1;
 			dir = Integer.parseInt(st.nextToken());
-			isValid = new int[4];
-			check(index, dir);
-			rotate(isValid);
+			isValid = new int[4]; // 회전하는 방향 저장
+			check(index, dir); // 회전해야하는 톱니바퀴 체크
+			rotate(isValid); // 톱니바퀴 회전
 		}
 		int result = calc();
 		System.out.println(result);
@@ -44,39 +44,39 @@ public class BJ_톱니바퀴 {
 	private static int calc() {
 		int result = 0;
 		for (int i = 0; i < 4; i++) {
-			if (gear[i].get(0) == 1) {
-				result += Math.pow(2, i);
+			if (gear[i].get(0) == 1) { // 12시 방향 톱니가 n일 경우
+				result += Math.pow(2, i); // 1번 톱니 바퀴일 경우 2^0, 2번 2^1, 3번 2^2, 4번 2^3
 			}
 		}
 		return result;
 	}
 	private static void rotate(int[] isValid) {
 		for (int i = 0; i < 4; i++) {
-			if(isValid[i] == 1) {
-				int temp = gear[i].pollLast();
-				gear[i].addFirst(temp);
-			}else if(isValid[i] == -1) {
-				int temp = gear[i].pollFirst();
-				gear[i].addLast(temp);
+			if(isValid[i] == 1) { // 회전 방향이 시계방향일 경우
+				int temp = gear[i].pollLast(); // list의 마지막 원소 가져오면서 삭제
+				gear[i].addFirst(temp); // list의 첫번째 원소로 추가
+			}else if(isValid[i] == -1) { // 회전 방향이 반시계방향일 경우
+				int temp = gear[i].pollFirst(); // list의 첫번째 원소 가져오면서 삭제
+				gear[i].addLast(temp); // list의 마지막 원소로 추가
 			}
 		}
 		
 	}
 	private static void check(int index, int dir) {
-		isValid[index] = dir;
+		isValid[index] = dir; 
 		
-		int prev = index - 1;
-		int post = index + 1;
+		int prev = index - 1; // 왼쪽 톱니바퀴
+		int post = index + 1; // 오른쪽 톱니바퀴
 		
-		if(prev >= 0 && isValid[prev] == 0) {
-			if(gear[prev].get(2) != gear[index].get(6)) {
-				check(prev, dir * -1);
+		if(prev >= 0 && isValid[prev] == 0) { // 왼쪽 톱니바퀴
+			if(gear[prev].get(2) != gear[index].get(6)) { // 왼쪽 2번 톱니와 현재 6번 톱니가 다를 경우 
+				check(prev, dir * -1); // 왼쪽 톱니바퀴로 가서 체크
 			}
 		}
 		
-		if(post <= 3 && isValid[post] == 0) {
-			if(gear[post].get(6) != gear[index].get(2)) {
-				check(post, dir * -1);
+		if(post <= 3 && isValid[post] == 0) { // 오른쪽 톱니바퀴
+			if(gear[post].get(6) != gear[index].get(2)) { // 오른쪽 6번 톱니와 현재 2번 톱니가 다를 경우
+				check(post, dir * -1); // 오른쪽 톱니바퀴로 가서 체크
 			}
 		}
 	}
